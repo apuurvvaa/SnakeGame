d81f7af7dc32e799e21c27916584919e7ad18265
Apurva Singh <101448367+apuurvvaa@users.noreply.github.com>
2022-04-13 13:58:42 -0700

    Update snake.cpp

--- snake.cpp
+++ snake.cpp
@@ -1,7 +1,65 @@
-<include iostream>
+#include <iostream>
 using namespace std;
 
+bool gameOver;
+const int width = 20;
+const int height = 20;
+int x, y, fruitX, fruitY, score; 
+enum Direction{STOP = 0, LEFT, RIGHT, UP, DOWN};
+Direction dir; 
+
+void Setup(){
+
+    gameOver = false;
+    dir = STOP;
+    x =  width /2;
+    y = height /2; 
+    fruitX = rand() % width;
+    fruitY = rand() % height; 
+    score = 0;
+}
+
+void Draw(){
+
+    system("cls"); 
+    for (int i = 0; i < width; i++)
+        cout << "#" << endl;
+
+
+    for (int i=0; i < height; i++)
+        for (int j = 0; j < width; j++)
+            {
+                if ( j ==0)
+                    cout << "#";
+        
+                    cout << " ";
+                if ( j == width -1 )
+                    cout << "#"; 
+
+            }
+
+
+    for (int i = 0; i < width +2; i++)
+        cout << "#";
+        cout << endl;
+
+
+}
+void Input(){
+
+}
+void Logic(){
+
+}
 int main(){
-  
-  return 0;
+
+    Setup();
+    while(!gameOver)
+    {
+        Draw();
+        Input();
+        Logic();
+        //Sleep(10); sleep(10);
+    }
+    return 0;
 }
